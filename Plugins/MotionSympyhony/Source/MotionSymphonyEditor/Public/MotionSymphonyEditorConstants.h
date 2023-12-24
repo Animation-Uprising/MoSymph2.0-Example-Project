37bb7358ccfe008886786cc153f6d176379e029f
Kenneth Claassen <claassen.ken@gmail.com>
2023-12-24 13:17:29 +0900

    Removed Plugins

--- Plugins/MotionSympyhony/Source/MotionSymphonyEditor/Public/MotionSymphonyEditorConstants.h
+++
@@ -1,9 +0,0 @@
-//Copyright 2020-2023 Kenneth Claassen. All Rights Reserved.
-
-#pragma once
-
-namespace MotionSymphonyEditorConstants
-{
-	const int32 ThumbnailSize = 128;
-	const int32 ThumnailSizeSmall = 16;
-}
